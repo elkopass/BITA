35231149f0b7142dc10448f107b3791baecc1c4f
Alexander Zorkin <somnoynadno@yandex.ru>
2022-05-09 23:03:03 +0700

    fix: use abs function for nano in Quatation

--- internal/trade/util/techan.go
+++ internal/trade/util/techan.go
@@ -20,13 +20,24 @@ func CandlesToTimeSeries(candles []*pb.HistoricCandle) *techan.TimeSeries {
 				End:   candles[i+1].Time.AsTime(),
 			},
 			Volume:     big.NewFromInt(int(c.Volume)),
-			OpenPrice:  big.NewFromString(fmt.Sprintf("%d.%d", c.Close.Units, c.Close.Nano)),
-			ClosePrice: big.NewFromString(fmt.Sprintf("%d.%d", c.Close.Units, c.Close.Nano)),
-			MaxPrice:   big.NewFromString(fmt.Sprintf("%d.%d", c.High.Units, c.High.Nano)),
-			MinPrice:   big.NewFromString(fmt.Sprintf("%d.%d", c.Low.Units, c.Low.Nano)),
+			OpenPrice:  big.NewFromString(fmt.Sprintf("%d.%d", c.Close.Units, abs(c.Close.Nano))),
+			ClosePrice: big.NewFromString(fmt.Sprintf("%d.%d", c.Close.Units, abs(c.Close.Nano))),
+			MaxPrice:   big.NewFromString(fmt.Sprintf("%d.%d", c.High.Units, abs(c.High.Nano))),
+			MinPrice:   big.NewFromString(fmt.Sprintf("%d.%d", c.Low.Units, abs(c.Low.Nano))),
 		}
 		techanCandles = append(techanCandles, tc)
 	}
 
 	return &techan.TimeSeries{Candles: techanCandles}
+}
+
+func abs(x int32) int32 {
+	return absDiff(x, 0)
+}
+
+func absDiff(x, y int32) int32 {
+	if x < y {
+		return y - x
+	}
+	return x - y
 }
