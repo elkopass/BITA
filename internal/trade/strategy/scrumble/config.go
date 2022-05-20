e923b33781b342c626fce98ec6a8c7164cfeb354
elkopass <tveretnov@mail.ru>
2022-05-20 16:27:04 +0700

    added  new SCRUMBLE strategy, using modified moving average

---
+++ internal/trade/strategy/scrumble/config.go
@@ -0,0 +1,29 @@
+package scrumble
+
+import (
+	"github.com/elkopass/BITA/internal/loggy"
+	"github.com/elkopass/BITA/internal/sdk"
+	"github.com/kelseyhightower/envconfig"
+)
+
+var services = sdk.NewServicePool()
+
+type TradeConfig struct {
+	LotsToBuy      int     `default:"1" split_words:"true"`
+	StopLossCoef   float64 `default:"0.97" split_words:"true"`
+	TakeProfitCoef float64 `default:"1.02" split_words:"true"`
+
+	MMAIntervalSeconds int `default:"6480000" split_words:"true"`
+
+	WorkerSleepDurationSeconds int `default:"30" split_words:"true"`
+}
+
+func NewTradeConfig() *TradeConfig {
+	var c TradeConfig
+	err := envconfig.Process("scrumble_strategy", &c)
+	if err != nil {
+		loggy.GetLogger().Sugar().Fatalf("failed to process config: %v", err)
+	}
+
+	return &c
+}
